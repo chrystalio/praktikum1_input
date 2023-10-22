b3836a3f2e46fdc6b7611c4181af09621dc179a2
Chrystalio <chrystalio.k@outlook.com>
2023-10-22 19:08:48 +0700

    Fix Unresolved References, Add TextWatcher and Update TextView on Text Change

--- app/src/main/java/com/example/praktikum1_input/MainActivity.kt
+++ app/src/main/java/com/example/praktikum1_input/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.praktikum1_input
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etTulisan: EditText
+    private lateinit var tvTulisan: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etTulisan = findViewById(R.id.et_tulisan) // Initialize the EditText
+        tvTulisan = findViewById(R.id.tv_tulisan) // Initialize the TextView
+
+        etTulisan.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // TODO: Implement what you want to do after text changes
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Do Nothing
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                tvTulisan.text = "Hello, " + p0.toString()
+            }
+        })
     }
 }
